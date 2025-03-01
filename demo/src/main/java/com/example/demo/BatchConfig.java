0c2c56fe4f2515918c83bcca1d17c3a1371155d7
info1 <info.mailaddress1@gmail.com>
2025-03-01 21:30:32 +0900

    csvReaderの分離

--- demo/src/main/java/com/example/demo/BatchConfig.java
+++ demo/src/main/java/com/example/demo/BatchConfig.java
@@ -1,66 +1,32 @@
 package com.example.demo;
 
 import com.example.demo.chunk.CsvProcessor;
+import com.example.demo.chunk.CsvReader;
 import com.example.demo.model.User;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.nio.charset.StandardCharsets;
-
 @Configuration
-@PropertySource("classpath:property/demo.properties")
 public class BatchConfig {
 
-//  @Autowired
-//  private CsvReader reader;
+  @Autowired
+  private CsvReader reader;
 
   @Autowired
   private CsvProcessor processor;
 
-  @Value("${csv.path}")
-  private String csvPath;
-
-  @Bean
-  @StepScope
-  public FlatFileItemReader<User> read() {
-    String[] nameArray = new String[]{"id", "name"};
-
-    return new FlatFileItemReaderBuilder<User>()
-            .name("csvReader")
-            .resource(new ClassPathResource(csvPath))
-            .linesToSkip(1)
-            .encoding(StandardCharsets.UTF_8.name())
-            .delimited()
-            .names(nameArray) // カラム名
-            .fieldSetMapper(new BeanWrapperFieldSetMapper<User>() {
-              {
-                setTargetType(User.class); // データを指定のクラスにバインド
-              }
-            })
-            .build();
-
-  }
-
-  @Bean
-  public Step demoStep(final JobRepository jobRepository, final PlatformTransactionManager transactionManager) {
+  private Step demoStep(final JobRepository jobRepository, final PlatformTransactionManager transactionManager) {
     return new StepBuilder("demoStep", jobRepository)
             .<User, User>chunk(10, transactionManager)
-            .reader(read())
+            .reader(reader.read())
             .processor(processor)
             .writer(items -> {})
             .build();
--- demo/src/main/java/com/example/demo/chunk/CsvReader.java
+++ demo/src/main/java/com/example/demo/chunk/CsvReader.java
@@ -6,12 +6,14 @@ import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 
 @Component
+@PropertySource("classpath:property/demo.properties")
 public class CsvReader {
 
   @Value("${csv.path}")
@@ -19,7 +21,7 @@ public class CsvReader {
 
   @StepScope
   public FlatFileItemReader<User> read() {
-    String[] nameArray = new String[]{"id", "name"};
+    final String[] nameArray = new String[]{"id", "name"};
 
     return new FlatFileItemReaderBuilder<User>()
             .name("csvReader")
