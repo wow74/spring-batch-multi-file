e8c4c7b65e142829e1f327b797a4044e6275ef24
info1 <info.mailaddress1@gmail.com>
2025-03-02 15:10:41 +0900

    不要なコードを削除

--- demo/src/main/java/com/example/demo/chunk/CsvReader.java
+++ demo/src/main/java/com/example/demo/chunk/CsvReader.java
@@ -4,11 +4,9 @@ import com.example.demo.model.User;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.MultiResourceItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.mapping.FieldSetMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -35,28 +33,23 @@ public class CsvReader {
   }
 
   private FlatFileItemReader<User> singleFileReader() {
-    CustomFlatFileItemReader<User> reader = new CustomFlatFileItemReader<>();
+    final CustomFlatFileItemReader<User> reader = new CustomFlatFileItemReader<>();
     reader.setName("csvReader");
     reader.setLinesToSkip(1);
     reader.setEncoding(StandardCharsets.UTF_8.name());
-    reader.setLineMapper(new DefaultLineMapper<User>() {{
-      setLineTokenizer(new DelimitedLineTokenizer() {{
-        setNames("id", "name");
-      }});
-      setFieldSetMapper(new CustomFieldSetMapper());
-    }});
+    reader.setLineMapper(new DefaultLineMapper<User>() {
+      {
+        setLineTokenizer(new DelimitedLineTokenizer() {{
+          setNames("id", "name");
+        }});
+        setFieldSetMapper(new BeanWrapperFieldSetMapper<User>() {
+          {
+            setTargetType(User.class);
+          }
+        });
+
+      }
+    });
     return reader;
-  }
-
-  private static class CustomFieldSetMapper implements FieldSetMapper<User> {
-    @Override
-    public User mapFieldSet(final FieldSet fieldSet) {
-      final User user = new User();
-      user.setId(fieldSet.readLong("id"));
-      user.setName(fieldSet.readString("name"));
-      user.setFileName(null); // ファイル名をセット
-      return user;
-    }
-
   }
 }
--- demo/src/main/java/com/example/demo/chunk/CustomFlatFileItemReader.java
+++ demo/src/main/java/com/example/demo/chunk/CustomFlatFileItemReader.java
@@ -1,48 +1,40 @@
 package com.example.demo.chunk;
 
 import com.example.demo.model.User;
-import lombok.Getter;
 import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.core.io.Resource;
-import org.springframework.lang.Nullable;
 
 import java.lang.reflect.Field;
 import java.util.Objects;
 
-@Getter
 public class CustomFlatFileItemReader<T extends User> extends FlatFileItemReader<User> {
   private String fileName;
 
   @Override
-  public void open(ExecutionContext executionContext) {
+  public void open(final ExecutionContext executionContext) {
     super.open(executionContext);
-    if (this.getCurrentResource() != null) {
-      fileName = this.getCurrentResource().getFilename();
-//      System.out.println("現在処理中のファイル: " + fileName);
-    }
+    fileName = Objects.nonNull(getCurrentResource()) ? getCurrentResource().getFilename() : null;
   }
 
   @Override
-  @Nullable
   public User read() throws Exception {
-     User item = this.doRead();
-    if (Objects.isNull(item)) {
-      return null;
-    }
-    System.out.println(getCurrentResource().getFilename());
-//    ((User) item).getId();
-    item.setFileName(fileName);
+    final User item = this.doRead();
+    if (Objects.isNull(item)) return null;
+    item.setFileName(fileName); // ファイル読み込み値以外をセット
     return item;
   }
 
-  public Resource getCurrentResource() {
+  private Resource getCurrentResource() {
     try {
-      Field resourceField = FlatFileItemReader.class.getDeclaredField("resource");
+      final Field resourceField = FlatFileItemReader.class.getDeclaredField("resource");
+
+      // privateフィールドにアクセスする
+      // VMでリフレクションの使用が禁止されている場合、SecurityExceptionを投げる
       resourceField.setAccessible(true);
       return (Resource) resourceField.get(this);
     } catch (NoSuchFieldException | IllegalAccessException e) {
-      throw new RuntimeException("Failed to access resource field", e);
+      throw new RuntimeException("リソースへのアクセスに失敗", e);
     }
   }
 
