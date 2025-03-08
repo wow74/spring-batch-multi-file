34b194e47c94a4ba0c0f1114a3a4f05e02c9b1ce
info1 <info.mailaddress1@gmail.com>
2025-03-08 15:18:20 +0900

    リポジトリ参考部分をコメント化

--- demo/src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ demo/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.batch.test.context.SpringBatchTest;
+//import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
