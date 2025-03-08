1531e46bc849149abeb57ade2b200b7718e464f3
info1 <info.mailaddress1@gmail.com>
2025-03-08 15:07:03 +0900

    リポジトリテストMSG追加

--- demo/src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ demo/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.batch.test.context.SpringBatchTest;
@@ -16,17 +18,28 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 // @SpringBatchTest // SpringBatchの機能(Tasklet, Chunkなど)をテストするときに使うアノテーション
 @SpringBootTest // RepositoryはBatch機能ではないためSpringBootTestアノテーションを使用
 @Slf4j
+@DisplayName("UserRepositoryTest")
 class UserRepositoryTest {
 
   @Autowired
   private UserRepository userRepository;
+
+  @BeforeAll
+  public static void beginTest() {
+    log.info("UserRepositoryTest 開始");
+  }
+
+  @AfterAll
+  public static void endTest() {
+    log.info("UserRepositoryTest 終了");
+  }
 
   @Test
   @Transactional
   @DisplayName("dummyBulkInsertTest")
   public void dummyBulkInsertTest() {
     // SETUP
-    log.info("開始");
+    log.info("dummyBulkInsertTest 開始");
     final List<User> expected = List.of(createUserData(1L), createUserData(2L));
 
     // WHEN
@@ -40,7 +53,7 @@ class UserRepositoryTest {
 
     // CLEANUP
     // @Transactionalでロールバック実行
-    log.info("終了");
+    log.info("dummyBulkInsertTest 終了");
 
   }
 
